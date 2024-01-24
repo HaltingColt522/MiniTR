839f0f5bf1d8ce7ee3e393821b97af3f787a115b
HaltingColt522 <l.frotschi@web.de>
2024-01-24 11:15:03 +0100

    Main

---
+++ unit1.pas
@@ -0,0 +1,99 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  ergebnis: real;
+  zahl1: integer;
+  zahl2: integer;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  zahl1 := strtoint(Edit1.Text);
+  zahl2 := strtoint(Edit2.text);
+  ergebnis := zahl1 + zahl2;
+
+
+  Label3.caption:=floattostr(ergebnis);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  zahl1 := strtoint(Edit1.Text);
+  zahl2 := strtoint(Edit2.text);
+  ergebnis := zahl1 - zahl2;
+
+
+  Label3.caption:=floattostr(ergebnis);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  zahl1 := strtoint(Edit1.Text);
+  zahl2 := strtoint(Edit2.text);
+  ergebnis := zahl1 * zahl2;
+
+
+  Label3.caption:=floattostr(ergebnis);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  zahl1 := strtoint(Edit1.Text);
+  zahl2 := strtoint(Edit2.text);
+  ergebnis := zahl1 / zahl2;
+
+
+  Label3.caption:=floattostr(ergebnis);
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+  zahl1 := strtoint(Edit1.Text);
+  zahl2 := strtoint(Edit2.text);
+  ergebnis := zahl1 / zahl2^2;
+
+
+  Label3.caption:=inttostr(ergebnis);
+end;
+
+end.
+
